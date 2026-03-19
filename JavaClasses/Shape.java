eb2cd363f6c922debfddc85184ce0f570f160491
KHisverycool <nnk547@york.ac.uk>
2026-03-19 15:05:01 +0000

    finished shape class

--- JavaClasses/Shape.java
+++ JavaClasses/Shape.java
@@ -1,8 +1,44 @@
-public class Shape {
+public class Shape { //Write comments
     private Coordinates position;
     private int sides;
 
-    public Shape(int noOfSides, Coordinates cord){
-
+    public Shape(int noOfSides, Coordinates coord){
+        this.sides = noOfSides;
+        this.position = coord;
     }
+
+    public Coordinates getCoordinates(){
+        return position;
+    }
+
+    public int getSides(){
+        return sides;
+    }
+
+    public void setCoordinates(Coordinates newcoord){
+        this.position=newcoord;
+    }
+
+    public void translate(int dx, int dy){
+        position.translate(dx,dy);
+    }
+
+    public void scale(int factor, boolean sign){
+        position.scale(factor,sign);
+    }
+
+    public double getArea(){
+        return 0.0; //place holder, since it will be overwritten later
+    }
+
+    public double getPerimeter(){
+        return 0.0; //Also a place holder
+    }
+
+    public String display(){
+        return "Shape at " + position.display();
+    }
+
+
 }
+
