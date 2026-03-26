b8adfa5723cd5c457381cf34afe7cd752b34c39e
KHisverycool <nnk547@york.ac.uk>
2026-03-26 15:30:09 +0000

    Add methods for shape management in Shape_List class

--- JavaClasses/Shape_List.java
+++ JavaClasses/Shape_List.java
@@ -6,8 +6,38 @@ public class Shape_List {
     public Shape_List(){
         listofShapes = new ArrayList<Shape>();
     }
-
     public void addShape(Shape s){
         listofShapes.add(s);
     }
+
+    public Shape removeShape(int pos){
+        if(pos >= 0 && pos < listofShapes.size()){
+            return listofShapes.remove(pos);
+        }
+        return null;
+    }
+
+    public Shape getShape(int pos){
+        if(pos>= 0 && pos<listofShapes.size()){
+            return listofShapes.get(pos);
+        }
+        return null;
+    }
+
+    public void translate(int dx, int dy) {
+        for (Shape shape : listofShapes) {
+            shape.translate(dx, dy);
+        }
+    }
+
+    public void scale(int factor, boolean sign) {
+        for (Shape shape : listofShapes) {
+            shape.scale(factor,sign);
+        }
+    }
+
+
+
+
+    
 }
